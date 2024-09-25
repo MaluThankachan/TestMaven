5dcbc2cd94e0d831d417b3dcb7924921cf4f0983
MaluL <MaluL@LAPTOP-G5IT7DRE>
2024-09-24 17:14:56 -0700

    commit 1

---
+++ TestMaven/src/main/java/Test/Test1.java
@@ -0,0 +1,18 @@
+package Test;
+
+public class Test1 {
+	int a=10;
+	int b=20;
+	public void result()
+	{
+		System.out.println(a+b);
+	}
+
+	public static void main(String[] args)
+	{
+		Test1 t = new Test1();
+		t.result();
+
+	}
+
+}
